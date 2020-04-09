8959fa62c60493a224b78e5e5651806245df2721
“mluena” <maluenarod@gmail.com>
2020-04-09 17:23:16 +0200

    project scaffolding

---
+++ src/components/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>i2i covid</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
