e8ab7c4a55cf6aa503524dbcd46c25b64009c703
“mluena” <maluenarod@gmail.com>
2020-04-15 09:43:12 +0200

    dropdown and hero without styles

--- src/components/dropdown/component.js
+++ src/components/dropdown/component.js
@@ -3,35 +3,40 @@ import proptypes from 'prop-types';
 import classnames from 'classnames';
 import Link from 'redux-first-router-link';
 
-const Dropdown = ({ info, children, isOpen }) => {
-  const [isActive, toggleDropdown] = useState(isOpen);
+const Dropdown = ({ info, option }) => {
+  const [isActive, toggleDropdown] = useState(false);
   const handleClick = () => {
     toggleDropdown(!isActive);
   };
-
-  return (<div className="c-dropdown">
-    {children}
-    <ul
-      className={classnames('dropdown-list', {
-        '-active': isOpen,
-      })}
-    >
-      {info.map((i) => (
-        <li key={i.iso} onClick={handleClick}>
-          <Link to={`/${i.iso}`}>{i.name}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>)
+  return (
+    <div className="c-dropdown">
+      <button onClick={handleClick}>{option.name}</button>
+      <ul
+        className={classnames('dropdown-list', {
+          '-active': isActive,
+        })}
+      >
+        {info.map((i) => (
+          <li key={i.iso} onClick={handleClick}>
+            <Link to={`/${i.iso}`}>{i.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 Dropdown.propTypes = {
-  info: proptypes.shape({
-    iso: proptypes.string.isRequired,
+  info: proptypes.arrayOf(
+    proptypes.shape({
+      iso: proptypes.string.isRequired,
+      name: proptypes.string.isRequired,
+    })
+  ),
+  option: proptypes.shape({
     name: proptypes.string.isRequired,
+    iso: proptypes.string.isRequired,
   }),
-  isOpen: proptypes.bool.isRequired,
-  children: proptypes.string.isRequired,
 };
 
 export default Dropdown;
--- src/components/hero/component.js
+++ src/components/hero/component.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Dropdown from 'components/dropdown';
 
@@ -7,21 +7,13 @@ import countries from './constants';
 const Hero = ({ iso }) => {
   const option = countries.find((country) => country.iso === iso);
   const options = countries.filter((country) => country.iso !== iso);
-  const [isOpen, toggleDropdown] = useState(false);
 
-  const handleClick = () => {
-    toggleDropdown(!isOpen);
-  };
   return (
     <div className="c-hero">
       <div className="hero-title">
         <h1>
           COVID19 status in
-          <Dropdown info={options} isOpen={isOpen}>
-            <button onClick={handleClick}>
-              <p>{option.name}</p>
-            </button>
-          </Dropdown>
+          <Dropdown info={options} option={option} />
         </h1>
       </div>
     </div>
