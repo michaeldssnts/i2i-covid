d3db16479861d64fdaccc3e0114edd7e2ee54eff
“mluena” <maluenarod@gmail.com>
2020-04-22 09:30:19 +0200

    component share

---
+++ src/components/share/component.js
@@ -0,0 +1,65 @@
+import React, { useState, useRef } from 'react';
+
+import Modal from 'components/modal';
+import Button from 'components/button';
+
+const Share = () => {
+  const inputEl = useRef();
+  const url = window.location.href;
+  const [{ isCopied, isOpen }, setState] = useState(
+    { isCopied: false, isOpen: false }
+    );
+
+  const handleClick = () => {
+    const { current } = inputEl;
+    current.select()
+
+    try {
+      document.execCommand('copy');
+      setState({ isCopied: true })
+      setTimeout(() => {
+        setState({ isCopied: false })
+        current.blur();
+      }, 3000);
+    } catch (err) {
+      console.warn('Oops, unable to copy');
+    }
+  };
+
+  const handleModal = () => {
+    setState({ isOpen: !isOpen })
+  }
+
+
+  return (
+    <div class="mapboxgl-ctrl mapboxgl-ctrl-group  map-share">
+      <button className="mapboxgl-ctrl-icon" onClick={handleModal}>
+        <Modal
+          isOpen={isOpen}
+          onRequestClose={() => setState({ isOpen: false })}
+        >
+          <div className="c-share">
+            <div className="share-content">
+              <h2>Copy link:</h2>
+              <div className="share-controls">
+                <input
+                  ref={inputEl}
+                  value={url}
+                  readOnly
+                />
+                <Button
+                  className="-background -primary -medium"
+                  onClick={handleClick}
+                >
+                  {isCopied ? 'Copied' : 'Copy'}
+                </Button>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      </button>
+    </div>
+  )
+};
+
+export default Share;
