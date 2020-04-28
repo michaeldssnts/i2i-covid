df72f0a530d9e43c7b4afbfe938ad66fa07df480
David Inga <david.inga@vizzuality.com>
2020-04-28 14:15:10 +0200

    summary removed container

--- src/components/summary/component.js
+++ src/components/summary/component.js
@@ -5,38 +5,36 @@ import Widgets from 'components/widgets';
 
 const Summary = ({ iso, categories }) => (
   <section className="c-summary">
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          {categories.map(({ name, slug, description }) => (
-            <article key={slug} className="summary-content">
-              <header>
-                <div className="row">
-                  <div className="col-sm-10 col-md-8 m-auto">
-                    <h2 className="text-center">{name}</h2>
-                    {description && <p>{description}</p>}
-                  </div>
+    <div className="row">
+      <div className="col-12">
+        {categories.map(({ name, slug, description }) => (
+          <article key={slug} className="summary-content">
+            <header>
+              <div className="row">
+                <div className="col-sm-10 col-md-8 m-auto">
+                  <h2 className="text-center">{name}</h2>
+                  {description && <p>{description}</p>}
                 </div>
-              </header>
+              </div>
+            </header>
 
-              <Widgets iso={iso} category={slug} filterBySummary={true} />
-              <aside className="summary-button">
-                <div className="row">
-                  <div className="col d-flex justify-content-center">
-                    <Link
-                      to={{
-                        type: 'COUNTRY',
-                        payload: { iso, category: slug },
-                      }}
-                    >
-                      Know more
-                    </Link>
-                  </div>
+            <Widgets iso={iso} category={slug} filterBySummary={true} />
+            <aside className="summary-button">
+              <div className="row">
+                <div className="col d-flex justify-content-center">
+                  <Link
+                    to={{
+                      type: 'COUNTRY',
+                      payload: { iso, category: slug },
+                    }}
+                  >
+                    Know more
+                  </Link>
                 </div>
-              </aside>
-            </article>
-          ))}
-        </div>
+              </div>
+            </aside>
+          </article>
+        ))}
       </div>
     </div>
   </section>
