7e11e884d621d96caa8b93aba2aae5780f41712b
David Inga <david.inga@vizzuality.com>
2020-04-29 12:28:57 +0200

    added grid to the footer

--- src/components/footer/component.js
+++ src/components/footer/component.js
@@ -11,51 +11,55 @@ import VizzualityImage from './partners/vizzuality.png';
 
 const Footer = () => (
   <footer className="c-footer">
-    <h2>Partners</h2>
-    <section>
-      <ul className="partners-list">
-        <li>
-          <a href="http://afr.rw/" title="Access to finance Rwanda">
-            <img className="partner-logo" src={AFRImage} alt="Access to finance Rwanda" />
-          </a>
-        </li>
-        <li>
-          <a href="https://fsduganda.or.ug/" title="FSDUganda">
-            <img className="partner-logo" src={FSDUImage} alt="FSDUganda" />
-          </a>
-        </li>
-        <li>
-          <a href="https://fsdkenya.org/" title="FSDKenya">
-            <img className="partner-logo" src={FSDKImage} alt="FSDKenya" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.fsdzambia.org/" title="FSDZambia">
-            <img className="partner-logo" src={FSDZImage} alt="FSDZambia" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.efina.org.ng/" title="Enhancing Financial Innovation and access">
-            <img className="partner-logo" src={EfinaImage} alt="Enhancing Financial Innovation and access" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.geopoll.com/" title="GeoPoll">
-            <img className="partner-logo" src={GeoPollImage} alt="GeoPoll" />
-          </a>
-        </li>
-        <li>
-          <a href="https://mathematica.org/" title="Mathematica">
-            <img className="partner-logo" src={MathematicaImage} alt="Mathematica" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.vizzuality.com/" title="Vizzuality">
-            <img className="partner-logo" src={VizzualityImage} alt="Vizzuality" />
-          </a>
-        </li>
-      </ul>
-    </section>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h2>Partners</h2>
+          <ul className="partners-list">
+            <li>
+              <a href="http://afr.rw/" title="Access to finance Rwanda">
+                <img className="partner-logo" src={AFRImage} alt="Access to finance Rwanda" />
+              </a>
+            </li>
+            <li>
+              <a href="https://fsduganda.or.ug/" title="FSDUganda">
+                <img className="partner-logo" src={FSDUImage} alt="FSDUganda" />
+              </a>
+            </li>
+            <li>
+              <a href="https://fsdkenya.org/" title="FSDKenya">
+                <img className="partner-logo" src={FSDKImage} alt="FSDKenya" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.fsdzambia.org/" title="FSDZambia">
+                <img className="partner-logo" src={FSDZImage} alt="FSDZambia" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.efina.org.ng/" title="Enhancing Financial Innovation and access">
+                <img className="partner-logo" src={EfinaImage} alt="Enhancing Financial Innovation and access" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.geopoll.com/" title="GeoPoll">
+                <img className="partner-logo" src={GeoPollImage} alt="GeoPoll" />
+              </a>
+            </li>
+            <li>
+              <a href="https://mathematica.org/" title="Mathematica">
+                <img className="partner-logo" src={MathematicaImage} alt="Mathematica" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.vizzuality.com/" title="Vizzuality">
+                <img className="partner-logo" src={VizzualityImage} alt="Vizzuality" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </footer>
 );
 
