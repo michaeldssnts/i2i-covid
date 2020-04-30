9b33670f675697a14e2d31726c20e060d3898849
“mluena” <maluenarod@gmail.com>
2020-04-30 10:42:05 +0200

    extra switch and Button removed

--- src/components/page-switch/component.js
+++ src/components/page-switch/component.js
@@ -1,35 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import NavLink from 'redux-first-router-link';
-import Button from 'components/button';
 
-const PageSwitch = ({ page, prev }) => (
-  <div className="c-resources">
-    {page === 'Resources' && (
-      <Button className="-border-color-1">
-        <NavLink to={{ type: prev.type, payload: prev.payload }} exact={true} strict={true}>
-          Country data
-        </NavLink>
-      </Button>
-    )}
+const PageSwitch = ({ page }) => (
+  <div className="c-switch">
     {page === 'Country' && (
-      <Button className="-border-color-1">
-        <NavLink to={{ type: 'RESOURCES', pathname: '/resources' }} exact={true} strict={true}>
-          Resources
-        </NavLink>
-      </Button>
+      <NavLink
+        className="c-button -border-color-1"
+        to={{ type: 'RESOURCES', pathname: '/resources' }}
+        exact={true}
+        strict={true}
+      >
+        Resources
+      </NavLink>
     )}
   </div>
 );
 
 PageSwitch.propTypes = {
-  prev: PropTypes.shape({
-    type: PropTypes.string,
-    payload: PropTypes.shape({
-      iso: PropTypes.string,
-      category: PropTypes.string,
-    }).isRequired,
-  }),
   page: PropTypes.string.isRequired,
 };
 
--- src/components/page-switch/index.js
+++ src/components/page-switch/index.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import Component from './component';
 
 const mapStateToProps = (state) => ({
-  prev: state.location.prev,
   page: state.page,
 });
 
