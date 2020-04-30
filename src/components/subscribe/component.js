86a2b61b66299488d3b3fbd1cc13790532f6ddd5
David Inga <david.inga@vizzuality.com>
2020-04-30 13:50:20 +0200

    subscribe with dynamic countries

--- src/components/subscribe/component.js
+++ src/components/subscribe/component.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
+import useAxios from 'axios-hooks';
 import Button from 'components/button';
 import Modal from 'components/modal';
 import Select from 'components/select';
-import countries from 'pages/home/constants'; //make dynamic when we get countries
+import Spinner from 'components/spinner';
+import { fetchCountries } from 'services/countries';
 
 const Subscribe = () => {
   const [isOpen, setState] = useState(false);
+  const [{ data, loading }] = useAxios(fetchCountries());
+  const countries = data && data.rows ? data.rows : null;
 
   const toggleModal = () => {
     setState(!isOpen);
   };
 
-  const countryOptions = countries.map(({ label, iso }) => {
+  const countryOptions = countries ? countries.map(({ country, iso }) => {
     return {
-      label,
+      label: country,
       value: iso,
     };
-  });
+  }) : [];
 
   return (
     <div className="c-subscribe">
@@ -37,45 +41,48 @@ const Subscribe = () => {
         )}
       >
         <div className="subscribe-modal">
-          <h3 color="#000">Get notified on new data upload</h3>
-          <form
-            id="subForm"
-            className="js-cm-form"
-            action="https://www.createsend.com/t/subscribeerror?description="
-            method="post"
-            data-id="30FEA77E7D0A9B8D7616376B900632311575FF58C7FD4C075C647EC7597F0FF1C0CE23F946F83D490D28C03B0F54880317A2210127DE612A79F4E709C3675BF2"
-          >
-            <div>
-              <div className="form-fields">
-                <label>Name </label>
-                <input aria-label="Name" id="fieldName" maxLength="200" name="cm-name" required />
+          <h3>Get notified on new data upload</h3>
+          {!data && loading && <Spinner />}
+          {data && !loading && (
+            <form
+              id="subForm"
+              className="js-cm-form"
+              action="https://www.createsend.com/t/subscribeerror?description="
+              method="post"
+              data-id="30FEA77E7D0A9B8D7616376B900632311575FF58C7FD4C075C647EC7597F0FF1C0CE23F946F83D490D28C03B0F54880317A2210127DE612A79F4E709C3675BF2"
+            >
+              <div>
+                <div className="form-fields">
+                  <label>Name </label>
+                  <input aria-label="Name" id="fieldName" maxLength="200" name="cm-name" required />
+                </div>
+                <div className="form-fields">
+                  <label>Email </label>
+                  <input
+                    autoComplete="Email"
+                    aria-label="Email"
+                    className="js-cm-email-input qa-input-email"
+                    id="fieldEmail"
+                    maxLength="200"
+                    name="cm-wjtuhy-wjtuhy"
+                    required
+                    type="email"
+                  />
+                </div>
+                <div className="form-fields">
+                  <label>Country </label>
+                  <Select
+                    name="cm-f-tjdudju"
+                    aria-label="Country"
+                    id="fieldtjdudju"
+                    maxLength="200"
+                    options={countryOptions}
+                    placeholder="Subscribe to an specific country"
+                  />
+                </div>
               </div>
-              <div className="form-fields">
-                <label>Email </label>
-                <input
-                  autoComplete="Email"
-                  aria-label="Email"
-                  className="js-cm-email-input qa-input-email"
-                  id="fieldEmail"
-                  maxLength="200"
-                  name="cm-wjtuhy-wjtuhy"
-                  required
-                  type="email"
-                />
-              </div>
-              <div className="form-fields">
-                <label>Country </label>
-                <Select
-                  name="cm-f-tjdudju"
-                  aria-label="Country"
-                  id="fieldtjdudju"
-                  maxLength="200"
-                  options={countryOptions}
-                  placeholder="Subscribe to an specific country"
-                />
-              </div>
-            </div>
-          </form>
+            </form>
+          )}
         </div>
       </Modal>
     </div>
